a08f2ba4cfea19b8edb13dfa0818b2bb228ba56b
Shuyang Liu <liushuyang2009@gmail.com>
2019-09-01 17:28:39 -0700

    update to be compatible with binutils-2.32

--- build/linux-amd64/bfd/bfdver.h
+++
@@ -1,4 +0,0 @@
-#define BFD_VERSION_DATE 20160125
-#define BFD_VERSION 226000000
-#define BFD_VERSION_STRING  "(GNU Binutils) " "2.26.20160125"
-#define REPORT_BUGS_TO "<http://www.sourceware.org/bugzilla/>"
